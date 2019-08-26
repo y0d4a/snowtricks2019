ba28c3c6babae03516b9c55289a31651ce4c9d3d
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-08-26 11:34:29 +0200

    Add Comment list View on Tricks
    Add Comment list View on Tricks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Form\TricksType;
 use App\Repository\TricksRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +19,15 @@ class HomeController extends AbstractController
      * @var TricksRepository
      */
     private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
 
-    public function __construct(TricksRepository $repository)
+    public function __construct(TricksRepository $repository, ObjectManager $em)
     {
         $this->repository = $repository;
+        $this->em = $em;
     }
     /**
      * @Route("/", name="home")
@@ -33,6 +42,32 @@ class HomeController extends AbstractController
             ['statut' => ['Publié', 'Edité']],
             ['dateUpdate' => 'DESC']
         );
-        return $this->render('pages/home.html.twig', array('tricks' => $tricks, 'current_menu' => 'home'));
+
+        foreach ($tricks as $trick){
+            $form = $this->createForm(TricksType::class, $trick, [
+                'action' => $this->generateUrl('trick.edit', ['id' => $trick->getId()])
+            ]);
+            $forms[] = $form->createView();
+
+            /* comment form */
+            $newComment = new Comment();
+            $formNewComment = $this->createForm(CommentType::class, $newComment, [
+                'action' => $this->generateUrl('comment.new', ['id' => $trick->getId()])
+            ]);
+            $formsNewComment[$trick->getId()] = $formNewComment->createView();
+
+            /* Comment view */
+            $comments[$trick->getId()][] = $this->em->getRepository(Comment::class)->findBy(
+                ['tricksId' => $trick->getId()]
+            );
+        }
+
+        return $this->render('pages/home.html.twig', array(
+            'tricks' => $tricks,
+            'current_menu' => 'home',
+            'formEdit' => $forms,
+            'formsNewComment' => $formsNewComment,
+            'comments' => $comments
+        ));
     }
 }
